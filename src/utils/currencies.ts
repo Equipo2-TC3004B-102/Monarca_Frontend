a1fe951c760ed37481ef756ddeec6707bd3572ac
SebastianBorjasLizardiTec <A01748052@tec.mx>
2026-04-20 18:24:27 -0600

    refactor(Frontend): Language change on currencies

--- src/utils/currencies.ts
+++ src/utils/currencies.ts
@@ -1,13 +1,13 @@
 export const currencyOptions = [
-  { id: "USD", name: "United States Dollar", banxico_id: "SF43718" },
+  { id: "USD", name: "Dólar Estadounidense", banxico_id: "SF43718" },
   { id: "EUR", name: "Euro", banxico_id: "SF46410" },
-  { id: "JPY", name: "Japanese Yen", banxico_id: "SF46406" },
-  { id: "GBP", name: "British Pound Sterling", banxico_id: "SF46407" },
-  { id: "CAD", name: "Canadian Dollar", banxico_id: "SF60632" },
-  { id: "CHF", name: "Swiss Franc", banxico_id: "SF46405" },
-  { id: "CNY", name: "Chinese Yuan", banxico_id: "SF290383" },
-  { id: "BRL", name: "Brazilian Real", banxico_id: "SF290312" },
-  { id: "ARS", name: "Argentine Peso", banxico_id: "SF290311" },
-  { id: "CLP", name: "Chilean Peso", banxico_id: "SF290351" },
-  { id: "COP", name: "Colombian Peso", banxico_id: "SF290382" }
+  { id: "JPY", name: "Yen Japonés", banxico_id: "SF46406" },
+  { id: "GBP", name: "Libra Esterlina", banxico_id: "SF46407" },
+  { id: "CAD", name: "Dólar Canadiense", banxico_id: "SF60632" },
+  { id: "CHF", name: "Franco Suizo", banxico_id: "SF46405" },
+  { id: "CNY", name: "Yuan Chino", banxico_id: "SF290383" },
+  { id: "BRL", name: "Real Brasileño", banxico_id: "SF290312" },
+  { id: "ARS", name: "Peso Argentino", banxico_id: "SF290311" },
+  { id: "CLP", name: "Peso Chileno", banxico_id: "SF290351" },
+  { id: "COP", name: "Peso Colombiano", banxico_id: "SF290382" }
 ];
